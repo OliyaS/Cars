71beb6ec8bd88895b5058aa885c5bd81929743e7
OliyaS <oliya_s@mail.ru>
2018-07-24 08:48:25 +0300

    k

--- src/test/java/cars/tests/RunnerTest.java
+++ src/test/java/cars/tests/RunnerTest.java
@@ -4,7 +4,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
 
 @CucumberOptions
-        (features = "src/test/java/cars/features/",
+        (features = "src/test/java/cars/features/",format = {"pretty", "html:target/cucumber","json:target/cucumber.json" },
      glue = "cars.steps")
 public class RunnerTest extends AbstractTestNGCucumberTests{
 }
